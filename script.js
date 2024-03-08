b3cf8bc7e2dbbe4cf3f50f95689e1f4b3a48e44a
TroLyAmazon <congdongamazon@gmail.com>
2024-03-08 08:55:03 +0700

    Update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentColorIndex = 0;
 
 function changeBorderColor() {
   currentColorIndex = (currentColorIndex + 1) % rainbowColors.length;
-  document.getElementById("BienGioi1").style.backgroundImage = `linear-gradient(to right, ${rainbowColors[currentColorIndex]}, ${rainbowColors[(currentColorIndex + 1) % rainbowColors.length]})`;
+  document.getElementById("BienGioiEnd").style.backgroundImage = `linear-gradient(to right, ${rainbowColors[currentColorIndex]}, ${rainbowColors[(currentColorIndex + 1) % rainbowColors.length]})`;
 }
 
 setInterval(changeBorderColor, 2000); // Thay đổi màu sau mỗi 2 giây (điều chỉnh theo nhu cầu)
